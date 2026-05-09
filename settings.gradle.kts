84576e523d8c975f29029b1efd3dfd0de4c187f3
Mher Shahinyan <mhershahinyan55@gmail.com>
2026-05-09 17:47:38 +0400

    create module structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,4 +24,27 @@ dependencyResolutionManagement {
 
 rootProject.name = "Weather"
 include(":app")
- 
+include(":app:app-domain")
+
+include(":feature")
+include(":feature:current")
+include(":feature:forecast")
+include(":feature:radar")
+include(":feature:current:current-domain")
+include(":feature:current:current-presentation")
+include(":feature:forecast:forecast-domain")
+include(":feature:forecast:forecast-presentation")
+include(":feature:radar:radar-presentation")
+include(":feature:radar:radar-domain")
+
+include(":data")
+include(":data:api")
+include(":data:datastore")
+include(":data:datastore:core")
+include(":data:datastore:repo")
+
+include(":common")
+include(":common:entity")
+include(":common:feature-api")
+include(":common:base-ui")
+include(":common:utils")
